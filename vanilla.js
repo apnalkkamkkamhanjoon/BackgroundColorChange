e5abfbc60ee646a518538d738fb6601bdd5f5d59
cutejoon <s22073@gsm.hs.kr>
2022-09-25 13:34:27 +0900

    0925

--- vanilla.js
+++
@@ -1,8 +0,0 @@
-let counter = 0;
-const button = document.getElementById("btn");
-const span = document.querySelector("span");
-function handleClick(){
-    counter = counter + 1;
-    span.innerText = `Total clicks :  ${counter}`;
-}
-button.addEventListener("click", handleClick);
